430c4a179a63eacfe64b81575aaa01408c2e522f
unknown <naikmahesh.naik@gmail.com>
2021-02-07 15:19:41 +0530

    update employeewage problem

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,24 +1,45 @@
+#!/bin/bash -x
+empCheck=$((RANDOM%3))
+isPartTime=2
+isFullTime=1
+empWagePerHour=20
+noOfWorkingDays=20
+maximumWorkingHours=100
+totalEmpHour=0
+totalWorkingDays=0
+declare -A dailyEmpWage
+function getEmployeeHours()
+{
+        empCheck=$1
+       case $empCheck in
+                $isPartTime)
+                      empHr=4
+                ;;
+                $isFullTime)
+                        empHr=8
+                ;;
+                *)
+                        empHr=0
+                ;;
+        esac
 
-echo "welcome to employee wage"
-ispresent=1;
-isparttime=2;
-wagePerhr=20;
-fullDay=8;
-dailyPayment=0;
-checkRandom=$(( RANDOM%2 ))
-if [ $ispresent -eq $checkRandom ]
-then
-	echo "Employee is Present"
-	dailyPayment=$(($wagePerhr*$fullDay ))
-	echo "daily payment is:" $dailyPayment
-elif [ $isparttime -eq $checkRandom ]
-	
-then
-	dailyPayment=$(($wagePerhr*$fullDay*$isparttime ))
-	echo "daily payment is:" $dailyPayment
+        echo $empHr
+}
+while [[ $totalEmpHour -le $maximumWorkingHours && $totalWorkingDays -le $noOfWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3))
+        empHr=$( getEmployeeHours $empCheck )
 
-else
-	echo "Employee is Absent"
-	echo "daily payment is:" $dailyPayment
-fi
+       dailyEmpWage["Day"$totalWorkingDays]=$((empWagepErHour*empHr))
 
+        totalEmpHour=$((totalEmpHour+empHr))
+	 totalEmpWage=$((totalEmpHour*empWagePerHour))
+done
+
+for i in "${!dailyEmpWage[@]}"
+do
+        echo "$i:${dailyEmpWage[$i]}"
+done
+echo "Total Working Hours of Employee in a Month is: "$totalEmpHour
+echo "Total Wage of Employee in a month is: "$totalEmpWage
